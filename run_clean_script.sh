25a115037e219132c9e5d462b721dfb94f7ef356
MariaTulubM <126815717+MariaTulubM@users.noreply.github.com>
2023-12-24 19:32:29 +0200

    Update run_clean_script.sh

--- run_clean_script.sh
+++ run_clean_script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Перевіряємо, чи передано правильну кількість аргументів
 if [ "$#" -ne 1 ]; then
     echo "Використання: $0 <шлях_до_директорії>"
     exit 1
@@ -8,5 +7,4 @@ fi
 
 target_directory="$1"
 
-# Запускаємо перший скрипт у новому терміналі
 gnome-terminal -- bash -c "./clean_directory.sh '$target_directory'; exec bash"
