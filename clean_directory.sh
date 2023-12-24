648b26f0a7a8621af5d415ac2716a5e200b60431
MariaTulubM <126815717+MariaTulubM@users.noreply.github.com>
2023-12-24 19:30:27 +0200

    Update clean_directory.sh

--- clean_directory.sh
+++ clean_directory.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Перевіряємо, чи передано правильну кількість аргументів
 if [ "$#" -ne 1 ]; then
     echo "Використання: $0 <шлях_до_директорії>"
     exit 1
@@ -8,10 +7,8 @@ fi
 
 target_directory="$1"
 
-# Пошук і видалення порожніх файлів з виведенням повідомлення
 find "$target_directory" -type f -empty -exec echo "Delete {}" \; -delete
 
-# Пошук і видалення порожніх директорій з виведенням повідомлення, за винятком кореневої директорії
 find "$target_directory" -type d -empty -not -path "$target_directory" -exec echo "Delete {}" \; -delete
 
 echo "Очищення завершено"
